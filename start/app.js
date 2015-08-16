83e2a2f0bf4869b1bc73b18dc62060371057b1ff
Arham Ahmed <a.arhamIT@gmail.com>
2015-08-16 14:39:53 -0400

    bug squashing

--- start/app.js
+++ start/app.js
@@ -2,20 +2,16 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var passport = require('passport');
-var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
+var passport = require('passport');
+//initialize mongoose schemas
+require('./models/models');
 var api = require('./routes/api');
-var authenticate = require('./routes/authenticate')(passport); //uses module w/a parameter
-var mongoose = require ('mongoose');
-
-//connect to mongodb
-mongoose.connect("mongodb://localhost/chirp-test");
-//initialize models
-require('./models/models.js');
-
+var authenticate = require('./routes/authenticate')(passport);
+var mongoose = require('mongoose');                         //add for Mongo support
+mongoose.connect('mongodb://localhost/test-chirp');              //connect to Mongo
 var app = express();
 
 // view engine setup
@@ -25,13 +21,9 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-
-//'middleware' - stands b/w routing and user requests
-app.use(session ({
-    secret: 'secret secret'
-
+app.use(session({
+  secret: 'insert secret here'
 }));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -39,43 +31,42 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 
-//initialize passport
-var initPassport = require('./passport-init');
-initPassport(passport);
-
-//mounting api router to api path
-app.use('/api', api);
 app.use('/auth', authenticate);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
+
+//// Initialize Passport
+var initPassport = require('./passport-init');
+initPassport(passport);
 
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- start/routes/api.js
+++ start/routes/api.js
@@ -1,64 +1,90 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
+var mongoose = require( 'mongoose' );
 var Post = mongoose.model('Post');
-
-//used for routes that must be authenticated
-function isAuthenticated(req, res, next) {
-
+//Used for routes that must be authenticated.
+function isAuthenticated (req, res, next) {
 	// if user is authenticated in the session, call the next() to call the next request handler 
 	// Passport adds this method to request object. A middleware is allowed to add properties to
 	// request and response objects
 
-	//allow all get request methods	
-	if (req.method === "GET") {
+	//allow all get request methods
+	if(req.method === "GET"){
+		return next();
+	}
+	if (req.isAuthenticated()){
 		return next();
 	}
 
-	if (req.isAuthenticated() {
-		return next();
-	})
-
-	//user is not authenticated... redirect to login page
+	// if the user is not authenticated then redirect him to the login page
 	return res.redirect('/#login');
-}
+};
 
-//regisering authentication middleware
+//Register the authentication middleware
 router.use('/posts', isAuthenticated);
+
 router.route('/posts')
+	//creates a new post
+	.post(function(req, res){
 
-	//get all posts
-	//TEMP SOLN
-	.get(function (req, res){ //request obj from client and response obj from server
-
-		res.send({message : 'TODO return all posts'});
-
+		var post = new Post();
+		post.text = req.body.text;
+		post.created_by = req.body.created_by;
+		post.save(function(err, post) {
+			if (err){
+				return res.send(500, err);
+			}
+			return res.json(post);
+		});
 	})
+	//gets all posts
+	.get(function(req, res){
+		console.log('debug1');
+		Post.find(function(err, posts){
+			console.log('debug2');
+			if(err){
+				return res.send(500, err);
+			}
+			return res.send(200,posts);
+		});
+	});
 
-	//TEMP SOLN
-	.post(function (req, res){
-		res.send({message: 'TODO create a new post'});
-	}) 
-
-//id is a parameter
+//post-specific commands. likely won't be used
 router.route('/posts/:id')
+	//gets specified post
+	.get(function(req, res){
+		Post.findById(req.params.id, function(err, post){
+			if(err)
+				res.send(err);
+			res.json(post);
+		});
+	}) 
+	//updates specified post
+	.put(function(req, res){
+		Post.findById(req.params.id, function(err, post){
+			if(err)
+				res.send(err);
 
-	
-	//modifies a particular post
-	.put(function (req, res) {
+			post.created_by = req.body.created_by;
+			post.text = req.body.text;
 
-		res.send({message: 'TODO modify post with ID: ' + req.params.id});
+			post.save(function(err, post){
+				if(err)
+					res.send(err);
+
+				res.json(post);
+			});
+		});
 	})
-
-	//returns a particular post
-	.get(function (req, res) {
-
-		res.send({message: 'TODO return post with ID: ' + req.params.id});
-	})
-
-	//delete existing post
-	.delete(function (req, res) {
-		res.send({message: 'TODO delete post with ID: ' + req.params.id});
+	//deletes the post
+	.delete(function(req, res) {
+		Post.remove({
+			_id: req.params.id
+		}, function(err) {
+			if (err)
+				res.send(err);
+			res.json("deleted :(");
+		});
 	});
 
 module.exports = router;
