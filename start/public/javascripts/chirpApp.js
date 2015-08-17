e01fc76c35a6d70eb1c4f23a4b8f46a7e3404d38
Arham Ahmed <a.arhamIT@gmail.com>
2015-08-17 19:30:50 -0400

    merging fronted with backend
    still need to fix login/register in angular to use functions in express

--- start/public/javascripts/chirpApp.js
+++ start/public/javascripts/chirpApp.js
@@ -1,50 +1,44 @@
-var app = angular.module('chirpApp', []);
+var app = angular.module('chirpApp', ['ngRoute']);
 
-//using the correct controller where necessary
 app.config(function($routeProvider){
-  $routeProvider
-    //the timeline display
-    .when('/', {
-      templateUrl: 'main.html',
-      controller: 'mainController'
-    })
-    //the login display
-    .when('/login', {
-      templateUrl: 'login.html',
-      controller: 'authController'
-    })
-    //the signup display
-    .when('/register', {
-      templateUrl: 'register.html',
-      controller: 'authController'
-    });
+	$routeProvider
+		//the timeline display
+		.when('/', {
+			templateUrl: 'main.html',
+			controller: 'mainController'
+		})
+		//the login display
+		.when('/login', {
+			templateUrl: 'login.html',
+			controller: 'authController'
+		})
+		//the signup display
+		.when('/register', {
+			templateUrl: 'register.html',
+			controller: 'authController'
+		});
 });
 
-app.controller('mainController', function($scope) {
-
+app.controller('mainController', function($scope){
 	$scope.posts = [];
-	$scope.newPost = {created_by : '', text : '', created_at: ''};
-
-	$scope.post = function () {
-
+	$scope.newPost = {created_by: '', text: '', created_at: ''};
+	
+	$scope.post = function(){
 		$scope.newPost.created_at = Date.now();
 		$scope.posts.push($scope.newPost);
-		$scope.newPost = {created_by : '', text : '', created_at: ''};
-	}
-
+		$scope.newPost = {created_by: '', text: '', created_at: ''};
+	};
 });
 
 app.controller('authController', function($scope){
-  	$scope.user = {username: '', password: ''};
-  	$scope.error_message = '';
+	$scope.user = {username: '', password: ''};
+	$scope.error_message = '';
 
 	$scope.login = function(){
-	    //placeholder until authentication is implemented
 		$scope.error_message = 'login request for ' + $scope.user.username;
 	};
 
 	$scope.register = function(){
-	    //placeholder until authentication is implemented
 		$scope.error_message = 'registeration request for ' + $scope.user.username;
 	};
 });
